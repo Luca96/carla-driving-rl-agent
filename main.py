c3a53e443dd516eda838a236b42377b4ce05d055
luca96 <luca.anzalone.dev@gmail.com>
2020-04-17 01:15:41 +0200

    (1) improved CARLA's environment class, (2) more experiments (problem changed to route follow), (3) keyboard agent + CARLAPlayEnvironment

--- main.py
+++ main.py
@@ -4,77 +4,17 @@ import pygame
 import logging
 import tensorflow as tf
 
-from agents.agents import Agents
-from agents.learn.experiments import *
-from agents.specifications import Specifications as Specs
+import tests
 
-from worlds import World
-from worlds.controllers import KeyboardController
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
 
 
-# TODO [project-level]: add assertions!!
+# TODO [project-level]: add assertions, docstrings, and annotate functions with types!!
 # TODO [project-level]: use os.path.join for directory strings!!
 # TODO [project-level]: use logger and different logging levels for debug, warnings, etc.!!
-
-def get_client(address='localhost', port=2000, timeout=2.0):
-    """Connects to the simulator.
-        @:returns a client object if the CARLA simulator accepts the connection.
-    """
-    client = carla.Client(address, port)
-    client.set_timeout(timeout)
-    return client
-
-
-def print_object(obj, message=None, filter=None):
-    if message is not None:
-        print(message)
-
-    for x in dir(obj):
-        if filter is None:
-            print(x)
-        elif filter in str(type(getattr(obj, x))):
-            print(x)
-
-
-def game_loop(vehicle='vehicle.audi.*', width=800, height=600):
-    pygame.init()
-    pygame.font.init()
-    world = None
-
-    try:
-        client = get_client(timeout=2.0)
-        display = pygame.display.set_mode((width, height), pygame.HWSURFACE | pygame.DOUBLEBUF)
-
-        world = World(client.get_world(), window_size=(width, height), actor_filter=vehicle)
-        controller = KeyboardController()
-        # controller = BasicAgentController(vehicle=world.player, destination=world.target_position.location)
-
-        clock = pygame.time.Clock()
-        while True:
-            clock.tick_busy_loop(30)
-
-            if controller.parse_events(client, world, clock, training=False):
-                return
-            # control = controller.act()
-            # world.apply_control(control)
-
-            world.tick(clock)
-            world.render(display)
-            pygame.display.flip()
-
-    finally:
-        if world and world.recording_enabled:
-            client.stop_recorder()
-
-        if world is not None:
-            world.destroy()
-
-        pygame.quit()
-
 
 def decay(value: float, rate: float, steps: int):
     for _ in range(steps):
@@ -91,18 +31,10 @@ if __name__ == '__main__':
     # ./CarlaUE4.sh -windowed -ResX=8 -ResY=8 -benchmark -fps=30
     # https://docs.unrealengine.com/en-US/Programming/Basics/CommandLineArguments/index.html
 
-    # game_loop()
-
-    # TODO: implement PretranAgent
-    # TODO: change problem from 'reach destination' to 'route follow'!
-
     num_episodes = 3
     batch_size = 256
     frequency = batch_size
     num_timesteps = batch_size * 3
-
-    # TODO: provide a base CARLA env class for a sync, async, pretrain environments..
-    # TODO: do a common CARLA world wrapper for the environments. (or a world_utils module)
 
     # experiment = CARLABaselineExperiment(window_size=(670, 500), debug=True)
     # experiment.train(agent=Agents.baseline(experiment, batch_size=512),
@@ -112,12 +44,17 @@ if __name__ == '__main__':
     # experiment.train(agent=Agents.evolutionary(experiment, 512, update_frequency=256, filters=40, exploration=0.1),
     #                  num_episodes=10, max_episode_timesteps=512, agent_name='evo', record_dir=None, load_agent=True)
 
-    for i in range(1, 100 + 1):
-        print(f'Trace-{i}')
-        experiment = CARLAPretrainExperiment(window_size=(670, 500), debug=True, vehicle_filter='vehicle.tesla.model3')
+    # Pretraining:
+    # for i in range(1, 100 + 1):
+    #     print(f'Trace-{i}')
+    #     experiment = CARLAPretrainExperiment(window_size=(670, 500), debug=True, vehicle_filter='vehicle.tesla.model3')
+    #
+    #     experiment.train(agent=Agents.dummy.random_walk(experiment, max_episode_timesteps=256, speed=30,
+    #                                                     traces_dir='data/traces/pretrain_tesla_batch256'),
+    #                      num_episodes=1, max_episode_timesteps=256, record_dir=None, weights_dir=None, load_agent=False)
 
-        experiment.train(agent=Agents.dummy.random_walk(experiment, max_episode_timesteps=256, speed=30,
-                                                        traces_dir='data/traces/pretrain_tesla_batch256'),
-                         num_episodes=1, max_episode_timesteps=256, record_dir=None, weights_dir=None, load_agent=False)
+    # tests.test_carla_env()
+    # tests.test_sequence_layer()
+    tests.test_keyboard_agent()
 
     pygame.quit()
